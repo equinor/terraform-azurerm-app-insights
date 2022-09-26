943f74c8d2a68ea9b4e41a4ad608ee4bbba9f4ac
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-09-26 14:17:29 +0200

    feat: update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,24 @@
-output "component_name" {
-  description = "The name of this Application Insights component."
-  value       = module.component.name
+output "component_ids" {
+  description = "A mapping of IDs for these Application Insights components."
+  value = {
+    for k, v in module.component : k => v.id
+  }
 }
 
-output "component_id" {
-  description = "The ID of this Application Insights component."
-  value       = module.component.id
+output "instrumentation_keys" {
+  description = "A mapping of instrumentation keys for these Application Insights components."
+  value = {
+    for k, v in module.component : k => v.instrumentation_key
+  }
+  sensitive = true
 }
 
-output "instrumentation_key" {
-  description = "The instrumentation key of this Application Insights component."
-  value       = module.component.instrumentation_key
-  sensitive   = true
-}
-
-output "connection_string" {
-  description = "The connection string of this Application Insights component."
-  value       = module.component.connection_string
-  sensitive   = true
+output "connection_strings" {
+  description = "A mapping of connection strings for these Application Insights components."
+  value = {
+    for k, v in module.component : k => v.connection_string
+  }
+  sensitive = true
 }
 
 output "smart_detection_action_group_name" {
