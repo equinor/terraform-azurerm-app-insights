a0795dcb30c79709f50b253f95835578e62f4fac
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-04-24 11:09:01 +0200

    feat: configure smart detection rules

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "azurerm_application_insights" "this" {
 
   tags = var.tags
 }
+
+resource "azurerm_application_insights_smart_detection_rule" "this" {
+  for_each = var.smart_detection_rules
+
+  name                               = each.value["name"]
+  application_insights_id            = azurerm_application_insights.this.id
+  enabled                            = true
+  send_emails_to_subscription_owners = true
+  additional_email_recipients        = []
+}
