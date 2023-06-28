0179cbaa53a0786d199cce63f88253c0899176e6
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-06-28 09:57:03 +0200

    feat!: migrate from smart detection rules to alert rules
    BREAKING CHANGE: remove variable `smart_detection_rules` and add
    variable `action_group_id`.
    
    Fixes #17

--- examples/standard-web-test/main.tf
+++ examples/standard-web-test/main.tf
@@ -23,6 +23,25 @@ module "log_analytics" {
   location            = azurerm_resource_group.this.location
 }
 
+resource "azurerm_monitor_action_group" "this" {
+  name                = "Application Insights Smart Detection"
+  resource_group_name = azurerm_resource_group.this.name
+  short_name          = "SmartDetect"
+  enabled             = true
+
+  arm_role_receiver {
+    name                    = "Monitoring Contributor"
+    role_id                 = "749f88d5-cbae-40b8-bcfc-e573ddc772fa"
+    use_common_alert_schema = true
+  }
+
+  arm_role_receiver {
+    name                    = "Monitoring Reader"
+    role_id                 = "43d0d8ad-25c7-4714-9337-8ba259a9fe05"
+    use_common_alert_schema = true
+  }
+}
+
 module "app_insights" {
   # source = "github.com/equinor/terraform-azurerm-app-insights"
   source = "../.."
@@ -31,6 +50,7 @@ module "app_insights" {
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   workspace_id        = module.log_analytics.workspace_id
+  action_group_id     = azurerm_monitor_action_group.this.id
 }
 
 module "standard_web_test" {
