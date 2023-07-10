b634f80a504aec1c8236aefe03370a6a91ff79db
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-07-10 10:07:41 +0200

    feat!: migrate from smart detection rules to alert rules (#19)
    BREAKING CHANGE: remove variable `smart_detection_rules` and add
    variable `action_group_id`.
    
    Fixes #17

--- variables.tf
+++ variables.tf
@@ -18,58 +18,9 @@ variable "workspace_id" {
   type        = string
 }
 
-variable "smart_detection_rules" {
-  description = "A map of smart detection rules to configure for this Application Insights component."
-
-  type = map(object({
-    name = string
-
-    enabled                            = optional(bool, true)
-    send_emails_to_subscription_owners = optional(bool, true)
-    additional_email_recipients        = optional(list(string), [])
-  }))
-
-  default = {
-    "slow_page_load_time" = {
-      name = "Slow page load time"
-    }
-
-    "slow_server_response_time" = {
-      name = "Slow server response time"
-    }
-
-    "long_dependency_duration" = {
-      name = "Long dependency duration"
-    }
-
-    "degradation_in_server_response_time" = {
-      name = "Degradation in server response time"
-    }
-
-    "degradation_in_dependency_duration" = {
-      name = "Degradation in dependency duration"
-    }
-
-    "degradation_in_trace_severity_ratio" = {
-      name = "Degradation in trace severity ratio"
-    }
-
-    "abnormal_rise_in_exception_volume" = {
-      name = "Abnormal rise in exception volume"
-    }
-
-    "potential_memory_leak_detected" = {
-      name = "Potential memory leak detected"
-    }
-
-    "potential_security_issue_detected" = {
-      name = "Potential security issue detected"
-    }
-
-    "abnormal_rise_in_daily_data_volume" = {
-      name = "Abnormal rise in daily data volume"
-    }
-  }
+variable "action_group_id" {
+  description = "The ID of the action group to send smart detector alerts to."
+  type        = string
 }
 
 variable "tags" {
--- versions.tf
+++ versions.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.39.0"
     }
+
+    azapi = {
+      source  = "azure/azapi"
+      version = ">= 1.0.0"
+    }
   }
 }
