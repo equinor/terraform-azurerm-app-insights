ed1498dabf6c0f053064de0d72aeff41d4e71a3b
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-03-06 15:18:59 +0100

    feat: set smart detector alert rule names

--- variables.tf
+++ variables.tf
@@ -23,6 +23,42 @@ variable "action_group_id" {
   type        = string
 }
 
+variable "dependency_performance_degradation_detector_rule_name" {
+  description = "A name for this dependency perfomance degradation detector alert rule."
+  type        = string
+  default     = null
+}
+
+variable "exception_volume_changed_detector_rule_name" {
+  description = "A name for this exception volume changed detector alert rule."
+  type        = string
+  default     = null
+}
+
+variable "failure_anomalies_detector_rule_name" {
+  description = "A name for this failure anomalies detector alert rule."
+  type        = string
+  default     = null
+}
+
+variable "memory_leak_detector_rule_name" {
+  description = "A name for this potential memory leak detector alert rule."
+  type        = string
+  default     = null
+}
+
+variable "request_performance_degradation_detector_rule_name" {
+  description = "A name for this response latency degradation detector alert rule."
+  type        = string
+  default     = null
+}
+
+variable "trace_severity_detector_rule_name" {
+  description = "A name for this trace severity detector alert rule."
+  type        = string
+  default     = null
+}
+
 variable "tags" {
   description = "A map of tags to assign to the resources."
   type        = map(string)
