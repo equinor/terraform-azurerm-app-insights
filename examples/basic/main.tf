7c24d0601dcaa62a0d71a730898cc94482fb699f
Ole Petter Gjøse <ogjo@equinor.com>
2024-03-07 09:47:29 +0100

    chore: update example file

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -38,8 +38,8 @@ resource "azurerm_monitor_action_group" "this" {
 }
 
 module "app_insights" {
-  # source = "github.com/equinor/terraform-azurerm-app-insights"
-  source = "../.."
+  source = "equinor/app-insights/azurerm"
+  version = "5.1.0"
 
   component_name      = "appi-${random_id.this.hex}"
   resource_group_name = var.resource_group_name
